c65f5bbeed23af2f98b6cb7589eee135d24ecd49
Anurag <anurag1342001@gmail.com>
2022-09-27 22:20:28 +0530

    updated code

---
+++ Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main(String[] args) {
+        int a = 45;
+        System.out.println(a);
+        int b = 045;
+        System.out.println(b);
+        int c = 0x45;
+        System.out.println(c);
+        int d = 0b00110010;
+        System.out.println(d);
+    }
+}
