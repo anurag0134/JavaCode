180399fc4093738d5ab1caae34d76d949375a4f3
Anurag Deshmukh <111648310+anurag0134@users.noreply.github.com>
2022-10-06 21:43:15 +0530

    java codes
    java codes

---
+++ Example1.java
@@ -0,0 +1,18 @@
+public class Example1 {
+    public static void main(String []args)
+    {
+        String s1="Raju";
+        String s2="Raju";
+        if (s1==s2)
+        {
+            System.out.println("References equal");
+
+        }
+        else
+        {
+            System.out.println("References are unequal");
+        }
+    }
+}
+
+
---
+++ Pattern12.java
@@ -0,0 +1,14 @@
+public class Pattern12
+{
+    public static void main(String []args)
+    {for (int j=1;j<=5;j++)
+        {
+         for (int i=5;i>=j;i--)
+
+            {
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+    }
+}
