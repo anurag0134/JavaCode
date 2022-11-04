7194ef93c89728c8ed66196d75f5112403fe1f93
Anurag Deshmukh <111648310+anurag0134@users.noreply.github.com>
2022-11-04 09:09:25 +0530

    Question 4

---
+++ Question4.java
@@ -0,0 +1,18 @@
+public class Question4
+{    public static void main(String []args)
+{
+    int i,j,n=1;
+    i=6;
+    while (i>=n)
+    {
+        j=5;
+        while(j<=i)
+        {
+            System.out.println();
+            j++;
+        }
+        System.out.print("*");
+        i--;
+    }
+}
+}
